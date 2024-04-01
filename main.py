74b6aa57760f4da51af12cb75235d098eceef92e
Daksh Dhaker <cs5221264@iitd.ac.in>
2024-04-01 15:07:13 +0530

    Change home screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+
 import pygame
 from constants import WINDOW_HEIGHT, WINDOW_WIDTH
-from main_menu import MainMenu
+from home import Home
 
 pygame.init()
 
@@ -8,11 +9,12 @@ window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Piano Master")
 
 running = True
-cur_screen = MainMenu()
+cur_screen = Home()
+
 while running:
     running = cur_screen.handle_events(pygame.event.get())
-    cur_screen.update()
-    cur_screen.render(window)
+    cur_screen.update() # This line changes the current state of game window but its effects are not visible. 
+    cur_screen.render(window) # This line will bring the current state onto the game_window
     if cur_screen.switch_screen:
         cur_screen = cur_screen.new_screen
     pygame.display.flip()
