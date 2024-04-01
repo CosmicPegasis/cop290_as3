73c80cde7e8e4d20750e5f79bba5b19c7db49cf3
Daksh Dhaker <cs5221264@iitd.ac.in>
2024-04-01 14:59:31 +0530

    Add barebones client

---
+++ room.py
@@ -0,0 +1,26 @@
+from menu import Menu
+import versus
+import multiplayer_game
+
+
+class Room(Menu):
+    def __init__(self):
+        super().__init__("assets/versus/background.mp3")
+        self.OPTIONS = ["Ready","Back"]
+
+        self.asset_man.load_sound("ready", "assets/room/ready.mp3")
+        self.asset_man.load_sound("back", "assets/back.mp3")
+
+    def update(self):
+        pass
+
+    def handle_selection(self, selected_option):
+        if selected_option == 0:
+            print("Ready")
+            self.switch_screen = True
+            self.new_screen = multiplayer_game.MultiplayerGame()
+        elif selected_option == 1:
+            self.switch_screen = True
+            self.new_screen = versus.Versus()
+        
+        return True
