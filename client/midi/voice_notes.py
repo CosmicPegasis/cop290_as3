3de8a1519b68c3bef13fc2d96cf7c28b75dbadac
Daksh Dhaker <cs5221264@iitd.ac.in>
2024-04-04 12:38:41 +0530

    Fix indentation

--- client/midi/voice_notes.py
+++ client/midi/voice_notes.py
@@ -1,6 +1,11 @@
 import pygame.mixer
 
-
+def _midi_to_note(self, midi_number):
+        note_names = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+        octave = (midi_number // 12) - 1
+        note = note_names[midi_number % 12]
+        return note + str(octave)
+    
 class VoiceNote:
     def __init__(self):
         self.d = {}
@@ -9,12 +14,7 @@ class VoiceNote:
             for j in range(len(notes)):
                 note_name = notes[j] + str(i)
                 self.d[note_name] = pygame.mixer.Sound(f"assets/{note_name}.mp3")
-
-    def _midi_to_note(self, midi_number):
-        note_names = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-        octave = (midi_number // 12) - 1
-        note = note_names[midi_number % 12]
-        return note + str(octave)
+    
 
     def play_note(self, midi_number):
-        self.d[self._midi_to_note(midi_number)].play()
+        self.d[_midi_to_note(midi_number)].play()
