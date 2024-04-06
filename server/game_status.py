87ab54340ce560ffeccd2d72360fa9b7dfbf1fc3
Daksh Dhaker <cs5221264@iitd.ac.in>
2024-04-06 12:00:21 +0530

    Add restart

--- server/game_status.py
+++ server/game_status.py
@@ -9,6 +9,7 @@ class GameStatus:
         self.moves = [None, None]  # here move is an int(This is score)
         self.wins = [0, 0]
         self.ties = 0
+        self.restart = False
 
     def get_player_move(self, p):
         return self.moves[p]
@@ -43,3 +44,6 @@ class GameStatus:
     def resetWent(self):
         self.p1Went = False
         self.p2Went = False
+        
+    def restart(self):
+        self.restart = True
