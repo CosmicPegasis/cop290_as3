d3c9edfb6c788f17b24e16a3b77447eefb3ad452
Aviral Singh <aviralsingh@protonmail.com>
2024-04-16 19:21:14 +0530

    improve scoring system

--- client/utils/score.py
+++ client/utils/score.py
@@ -1,13 +1,25 @@
 def calc_score(to_score, reference, note_length):
     score = 0
+    if len(to_score) == 0 or len(reference) == 0:
+        return 0
+    delay = to_score[0][2]
+    for i in range(len(to_score)):
+        to_score[i][2] = to_score[i][2] - delay
+    delay = reference[0][2]
+    for i in range(len(reference)):
+        reference[i][2] = reference[i][2] - delay
+    print(f"to_score:\n{to_score}\n")
+    print(f"reference:\n{reference}\n")
     for i in range(len(to_score)):
         to_score[i][2] /= note_length
-    for i in range(min(len(to_score), len(reference))):
-        if to_score[i][1] == reference[i][1] and to_score[i][0] == reference[i][0]:
-            score += 50
-            if to_score[i][2] <= (reference[i][2] + 200) or to_score[i][2] >= (
-                reference[i][2] - 200
+    for elem_to_score in to_score:
+        for elem_reference in reference:
+            if (
+                elem_to_score[0] == elem_reference[0]
+                and elem_to_score[1] == elem_reference[1]
+                and abs(elem_to_score[2] - elem_reference[2]) <= 2000
             ):
-                score += 25
+                score += 100
+                break
 
     return score
