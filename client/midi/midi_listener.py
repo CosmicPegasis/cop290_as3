4b3b432ffe88185c5a686f1cc3774dcd64ae7522
Daksh Dhaker <cs5221264@iitd.ac.in>
2024-04-19 16:32:55 +0530

    disable MIDI audio

--- client/midi/midi_listener.py
+++ client/midi/midi_listener.py
@@ -43,10 +43,10 @@ class MidiListener:
                     note_on = True if event.status // 16 == 9 else False
                     note_off = True if event.status // 16 == 8 else False
                     if note_on:
-                        self.midi_output.note_on(note, velocity)
+                        # self.midi_output.note_on(note, velocity)
                         self.cur_events.append(["on", note, event.timestamp])
                     elif note_off:
-                        self.midi_output.note_off(note)
+                        # self.midi_output.note_off(note)
                         self.cur_events.append(["off", note, event.timestamp])
 
     def is_listening(self):
